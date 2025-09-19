2bd605f2509a13ca090e1cda77e3458a5ba8149f
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-09-19 15:46:04 +0700

    [update]-<component>-product_list_page
    - ใช้ fetchProduct จาก product_service
    - ดึงรายการสินค้าจาก product_service
    - แสดงรายการสินค้า*testing

--- src/app/pages/product/product-list/product-list.ts
+++ src/app/pages/product/product-list/product-list.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ProductModel } from '../../../models/product-model';
+import { ProductService } from '../../../services/product-service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
-export class ProductList {
+export class ProductList implements OnInit {
+  constructor(private productService:ProductService){}
 
+  ngOnInit(): void {
+    this.loadProduct()
+  }
+
+  baseProducts:ProductModel[] = []
+
+  loadProduct(){
+    this.productService.fetchProduct().subscribe()
+    this.productService.originProduct$.subscribe(
+      {
+        next:(res) => {this.baseProducts = res},
+        error:(err) => console.error("ERROR IS " + err)
+      }
+    )
+  }
 }
