3a09731629fa5319c0e878d5c60487aaf35b8e22
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-09-19 15:39:08 +0700

    [update]-<service>-product_service
    - สร้างตัวกลางรายการสินค้าจาก backend
    - method ดึงข้อมูลสินค้าจาก backend

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ProductModel } from '../models/product-model';
+import { Observable,BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  
+  constructor(private http:HttpClient){}
+  private API_URL = 'http://localhost:3000/products'
+
+  private originProduct = new BehaviorSubject<ProductModel[]>([])
+  originProduct$ = this.originProduct.asObservable()
+
+  //#region API
+  fetchProduct():Observable<ProductModel[]>{
+    return this.http.get<ProductModel[]>(this.API_URL).pipe(
+      tap(res => this.originProduct.next(res))//res คือ ข้อมูลที่ดึงมาจาก backend
+    )
+  }
+  //#endregion
 }
