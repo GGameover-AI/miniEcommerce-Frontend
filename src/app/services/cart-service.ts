c05433412b5f16c8cf988122fb634921eb7de12b
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-09-25 11:39:15 +0700

    [feature]-<service>-cart_service
    เพิ่มสินค้าในตะกร้า

--- src/app/services/cart-service.ts
+++ src/app/services/cart-service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ProductModel } from '../models/product-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  
+  private cartStock = new BehaviorSubject<ProductModel[]>([])
+  cartStock$ = this.cartStock.asObservable()
+
+  addToCart(newProduct:ProductModel){
+    const currentCart = this.cartStock.value
+    const validProduct = currentCart.find(e => e.id === newProduct.id)
+
+    if(validProduct){
+
+      validProduct.quantity += 1
+      this.cartStock.next([...currentCart])
+
+    }else{
+      this.cartStock.next([...currentCart,{...newProduct,quantity:1}])
+    }
+
+    console.log("Products In Cart : ",this.cartStock.value)
+  }
 }
