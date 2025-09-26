6471c0cc2eb36bdfe7e5b3a329a1cd3568bd58cc
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-09-26 18:37:06 +0700

    [update]-<routing>-app_routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ProductList } from './pages/product/product-list/product-list';
 import { ProductDetail } from './pages/product/product-detail/product-detail';
 import { Profile } from './pages/user/profile/profile';
 import { Cart } from './pages/order/cart/cart';
+import { Checkout } from './pages/order/checkout/checkout';
 
 export const routes: Routes = [
     {path:'',component:ProductList},
     {path:'detail/:id',component:ProductDetail},
     {path:'cart',component:Cart},
+    {path:'payment',component:Checkout},
     {path:'profile/:id',component:Profile},
 ];
