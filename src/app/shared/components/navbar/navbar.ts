6457bdf02e927a56009c3225ed8e736febd19a17
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-09-29 20:56:07 +0700

    [feature]-<component>-navbar
    -ระบบค้นหาสินค้า

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { ProductService } from '../../../services/product-service';
+import { ProductModel } from '../../../models/product-model';
+import { FormControl,ReactiveFormsModule } from '@angular/forms';
+import { NgFor,CurrencyPipe,NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule,NgFor,NgIf,CurrencyPipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar {
-  userID:number = 1
+export class Navbar implements OnInit {
+
+  constructor(private productService: ProductService,private router:Router) { }
+  ngOnInit(): void {
+    this.loadProduct()
+    this.searchObserver()
+  }
+
+  baseProduct: ProductModel[] = []
+  filteredProduct:ProductModel[] = []
+  userID: number = 1
+
+  searchBox = new FormControl('')
+
+  filterKeyword(keyword:string){
+    keyword = keyword.toLowerCase()
+    this.filteredProduct = this.baseProduct.filter(p => p.name.toLowerCase().includes(keyword)&& p.quantity !== 0)
+  }
+
+  searchObserver(){
+    this.searchBox.valueChanges.subscribe(
+      keyword => {this.filterKeyword(keyword ?? '')}
+    )
+  }
+
+  onSearchProduct(id:string){
+    this.router.navigate(['/detail',id])
+    this.searchBox.setValue('')
+    console.log('Card Click!')
+  }
+
+  loadProduct() {
+    this.productService.originProduct$.subscribe(
+      res => {
+        this.baseProduct = res
+        this.filteredProduct = res
+      }
+
+    )
+  }
 }
