438da06a56c83f0247e4c3fdf513ae5d3ebafdb2
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-10-03 17:28:25 +0700

    [update]-<component>-login_page

--- src/app/pages/user/login/login.ts
+++ src/app/pages/user/login/login.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../services/auth-service';
+import { UserLoginModel } from '../../../models/user-login-model';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  constructor(private authService: AuthService) { }
 
+  formLogin = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  usernameBearer: string = this.formLogin.get('username')?.value as string
+  passwordBearer: string = this.formLogin.get('password')?.value as string
+
+  loginPayload: UserLoginModel = { "username": this.usernameBearer, "password": this.passwordBearer }
+
+  onLogin() {
+    if (this.formLogin.valid) {
+      this.authService.login(this.loginPayload).subscribe(
+        {
+          error: (err) => alert(err.error?.message ||"เกิดข้อมผิดพลาด")
+        })
+    }
+  }
 }
--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -21,6 +21,7 @@ export class AuthService {
         res => {
           if(res && res.token){
             sessionStorage.setItem(this.tokenKey,res.token)
+            this.route.navigate([''])
           }
         }
       )
