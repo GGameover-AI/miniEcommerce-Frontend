506f332f6a7b6101539b9fb3dbd114460f39c484
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-10-03 15:02:46 +0700

    [update]-<component>-app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,17 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./shared/components/navbar/navbar";
 import { ProductService } from './services/product-service';
+import { NgIf } from '@angular/common';
+import { AuthService } from './services/auth-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar,NgIf],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
-  constructor(private productService:ProductService){}
+  constructor(private productService:ProductService,private authService:AuthService){}
 
   ngOnInit(): void {
       this.productService.fetchProduct() // เรียก api ดึงรายการสินค้าจาก backend
+  }
+
+  get loginCheck():boolean{
+    return this.authService.isLogin()
   }
 }
