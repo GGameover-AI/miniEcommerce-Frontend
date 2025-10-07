d9a385cc66d285d59bf931e6991848fdcc5ea766
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-10-07 19:34:44 +0700

    [update]-<component>-profile_page

--- src/app/pages/user/profile/profile.ts
+++ src/app/pages/user/profile/profile.ts
@@ -1,33 +1,52 @@
-import { NgIf,NgFor,CurrencyPipe } from '@angular/common';
+import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth-service';
+import { OrderService } from '../../../services/order-service';
+import { OrderHistoryModel } from '../../../models/order-history-model';
+import { UserInfoModel } from '../../../models/user-info-model';
 
 @Component({
   selector: 'app-profile',
-  imports: [NgIf,NgFor,CurrencyPipe],
+  imports: [NgIf, NgFor, CurrencyPipe],
   templateUrl: './profile.html',
   styleUrl: './profile.css'
 })
-export class Profile implements OnInit{
+export class Profile implements OnInit {
 
-  constructor(private authService:AuthService){}
+  constructor(private authService: AuthService, private orderService: OrderService) { }
 
   ngOnInit(): void {
-      
+    this.loadProfile()
+    this.loadOrderHistory()
   }
 
-  orderList = [
-    {"id":1,"name":"g","price":120},
-    {"id":2,"name":"Ai","price":500}
-  ]
+  orderList: OrderHistoryModel[] = []
+  userInfo: UserInfoModel = { sub: '', unique_name: '', email: '' }
 
-  orderListWithExpand = this.orderList.map(e => ({...e,expand:false}))
+  // ถ้าไม่ประกาศ (OrderHistoryModel & {expand:boolean})[] expend ใน onExpend จะหาไม่เจอ
+  orderListWithExpand:(OrderHistoryModel & {expand:boolean})[] = [] 
 
-  onExpend(index:number){
+  onExpend(index: number) {
     this.orderListWithExpand[index].expand = !this.orderListWithExpand[index].expand
   }
 
-  onLogout(){
+  onLogout() {
     this.authService.logout()
+  }
+
+  loadProfile() {
+    this.userInfo = this.authService.getUserInfo()
+  }
+
+  loadOrderHistory() {
+    this.orderService.orderHistory$.subscribe(
+      {
+        next: (res) => { 
+          this.orderList = res 
+          this.orderListWithExpand =  this.orderList.map(e => ({ ...e, expand: false }))
+        },
+        error: (err) => console.log(err)
+      }
+    )
   }
 }
