a59b0942eaf04d1de942d121a8531a8bf208782d
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-10-08 09:12:05 +0700

    [update]-<model>-order_model

--- src/app/models/order-model.ts
+++ src/app/models/order-model.ts
@@ -2,11 +2,11 @@ import { ProductModel } from "./product-model";
 
 export interface OrderModel {
     recipient:string
-    phone:number
+    phone:string
     email?:string
     address:string
     paymentType:string
-    cardNumber:number
+    cardNumber:string
     cardHolder:string
     cardExp:string
     cvc:number
