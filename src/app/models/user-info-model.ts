cafc1c9d86a977c97bb506437ccbd8e860038c3a
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-10-08 09:43:25 +0700

    [update]-<model>-user_info_model

--- src/app/models/user-info-model.ts
+++ src/app/models/user-info-model.ts
@@ -1,5 +1,5 @@
 export interface UserInfoModel {
-    sub:string
-    unique_name:string
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier":string
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name":string
     email:string
 }
