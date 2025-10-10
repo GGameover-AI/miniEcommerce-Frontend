05e404b79be7e66f80f43b67537383820a42b959
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-10-10 18:33:32 +0700

    [update]-<component>-loading

--- src/app/shared/components/loading/loading.ts
+++ src/app/shared/components/loading/loading.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './loading.css'
 })
 export class Loading {
+  @Input() message!:string
+  @Input() status!:boolean
 
 }
