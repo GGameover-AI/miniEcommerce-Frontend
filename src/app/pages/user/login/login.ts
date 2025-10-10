1381d39fc7c603f9f41cd79a3f2ff370b5be7cc3
GGameover <212758652+GGameover-AI@users.noreply.github.com>
2025-10-10 19:31:11 +0700

    [update]-<component>-login

--- src/app/pages/user/login/login.ts
+++ src/app/pages/user/login/login.ts
@@ -3,15 +3,19 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth-service';
 import { UserLoginModel } from '../../../models/user-login-model';
+import { Loading } from "../../../shared/components/loading/loading";
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, Loading],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
   constructor(private authService: AuthService) { }
+
+  messageLoading: string = 'กำลังล็อกอิน'
+  isLoading: boolean = false
 
   formLogin = new FormGroup({
     username: new FormControl('', [Validators.required]),
@@ -19,16 +23,22 @@ export class Login {
   })
 
   onLogin() {
+
     if (this.formLogin.valid) {
+      this.isLoading = true
       const loginPayload: UserLoginModel = {
-              "username": this.formLogin.get('username')?.value as string,
-              "password": this.formLogin.get('password')?.value as string,
-            }
+        "username": this.formLogin.get('username')?.value as string,
+        "password": this.formLogin.get('password')?.value as string,
+      }
 
 
       this.authService.login(loginPayload).subscribe(
         {
-          error: (err) => alert(err.error?.message || "เกิดข้อมผิดพลาด")
+          next: () => { this.isLoading = false },
+          error: (err) => {
+            this.isLoading = false
+            alert(err.error?.message || "เกิดข้อผิดพลาด:Server อาจอยู่ในโหมด Sleep ZZzzz...")
+          }
         })
     }
   }
